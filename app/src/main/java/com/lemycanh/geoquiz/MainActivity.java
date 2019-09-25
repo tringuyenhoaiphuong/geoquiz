7c3dd7311ff0ddd109ce33ac21db16b369e6b1db
Canh Le My <lemycanh@gmail.com>
2019-09-25 08:34:52 +0700

    * Activity Life Cycle

--- app/src/main/java/com/lemycanh/geoquiz/MainActivity.java
+++ app/src/main/java/com/lemycanh/geoquiz/MainActivity.java
@@ -1,6 +1,7 @@
 package com.lemycanh.geoquiz;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -14,6 +15,8 @@ import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    final String TAG = "GeoQuiz";
 
     ArrayList<Question> mQuestionList;
     int mCurrentQuestionIndex;
@@ -63,10 +66,56 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate called");
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
         loadQuestion();
+        showQuestion();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart called");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop called");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy called");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause called");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume called");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.d(TAG, "onSaveInstanceState called");
+        outState.putInt("QuestionIndex", mCurrentQuestionIndex);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        Log.d(TAG, "onRestoreInstanceState called");
+        mCurrentQuestionIndex = savedInstanceState.getInt("QuestionIndex");
         showQuestion();
     }
 
